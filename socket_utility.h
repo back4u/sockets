36ccb1287a30d5704a90f0d42f8ccf7eed654ab3
back4u <38805342+back4u@users.noreply.github.com>
2019-03-08 14:50:35 +0300

    Update socket_utility.h

--- socket_utility.h
+++ socket_utility.h
@@ -11,16 +11,16 @@
 #ifndef SOCKET_UTILITY_H
 #define SOCKET_UTILITY_H
 
-int create_recv_socket(char *local_ip, char *local_port, int local_socktype);
+int32_t create_recv_socket(char *local_ip, char *local_port, int32_t local_socktype);
 
-int create_send_socket(char *local_ip, char *local_port, int local_socktype,
-		               char *remote_ip, char *remote_port, int remote_socktype);
+int32_t create_send_socket(char *local_ip, char *local_port, int32_t local_socktype,
+		               char *remote_ip, char *remote_port, int32_t remote_socktype);
 
-int get_eth_address(char *eth_name, char *eth_addr);
+int32_t get_eth_address(char *eth_name, char *eth_addr);
 
-int create_recv_socket_eth(char *eth_name, char *local_port, int local_socktype);
+int32_t create_recv_socket_eth(char *eth_name, char *local_port, int32_t local_socktype);
 
-int create_send_socket_eth(char *local_eth_name, char *local_port, int local_socktype,
-		                   char *remote_eth_name, char *remote_port, int remote_socktype);
+int32_t create_send_socket_eth(char *local_eth_name, char *local_port, int32_t local_socktype,
+		                   char *remote_eth_name, char *remote_port, int32_t remote_socktype);
 
 #endif /* SOCKET_UTILITY_H */
